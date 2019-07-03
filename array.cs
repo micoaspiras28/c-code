a45c9f6a583711d7693b6a664abeb8645beed4f5
Mico <qmichael1234@gmail.com>
2019-07-03 16:13:27 +0800

    Set up

---
+++ array.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array{
+  class Program{
+    static void Main(string[] args){
+      
+    }
+  }
+}
