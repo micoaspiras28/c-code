9543a922c6fea0f23388cc9cfa0a8012d47eec91
Mico <qmichael1234@gmail.com>
2019-07-03 16:29:26 +0800

    First Commit

---
+++ methods.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods{
+
+  class Program{
+
+    static void Main(string[] args){
+      //This methods allow user to say hi!
+
+      Console.Readline();
+    }
+
+    static void SayHi(){
+
+    }
+  }
+}
