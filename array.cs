0e262cbfb961381b8f376bb34ace86961e3893e6
Mico <qmichael1234@gmail.com>
2019-07-03 16:26:05 +0800

    Final array

--- array.cs
+++ array.cs
@@ -7,7 +7,19 @@ using System.Threading.Tasks;
 namespace array{
   class Program{
     static void Main(string[] args){
-      
+
+      int[] luckyNumbers = {9, 10, 5, 7, 4, 3, 2, 39};
+      string[] friends = new string[4];
+      friends[0] = "Mico";
+      friends[1] = "John";
+      friends[2] = "Quintero";
+      friends[3] = "Michael";
+
+      luckyNumbers[1] = 90; //Change array value
+      Console.WriteLine(luckyNumbers[0]); //Print the value of array
+
+      Console.Readline();
     }
+
   }
 }
