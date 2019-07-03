707ca59dcd15c9830fb71039979c9dab7f22e2be
Mico <qmichael1234@gmail.com>
2019-07-03 16:53:16 +0800

    unfinised

---
+++ ifStatement.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods{
+
+  class Program{
+
+    static void Main(string[] args){
+
+      bool isMale = true;
+      bool isTall = true;
+
+      if (isMale && isTall ){
+        Console.WriteLine("You a");
+      }
+    }
+  }
+}
